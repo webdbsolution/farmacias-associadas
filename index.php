0ef96aa8a1bec44e5b9dc2321173001aca1f802b
webdbsolution <suporte1@webdbsolution.com.br>
2023-01-27 13:09:42 -0300

    up

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?include("includes/scripts.php")?>
+    <!-- css  -->
     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/variaveis.css">
     <title>Farmácias Associadas</title>
 </head>
 <body>
--- modulos/head.php
+++ modulos/head.php
@@ -1,17 +1,17 @@
 <header id="topo">
     <div class="row m-0">
-        <div class="col-lg-3">
-            <img src="imagens/logo-topo.png" alt="" class="img-fluid m-3">
+        <div class="col-lg-3 my-3">
+            <img src="imagens/logo-topo.png" alt="" class="img-fluid">
         </div>
-        <div class="col-lg-8 text-right">
-            <nav>
+        <div class="col-lg-9 text-right">
+            <nav class="mt-4 roboto">
                 <a href="">Home</a>
                 <a href="">Produtos</a>
                 <a href="">Quem somos</a>
                 <a href="">Localização</a>
                 <a href="">Contato</a>
-                <a href=""><i class="fa-brands fa-facebook"></i></a>
-                <a href=""><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://www.facebook.com/farmaciasassociadaspiratini" target="_blank" class="m-1"><i class="fa-brands fa-facebook icone"></i></a>
+                <a href="https://www.instagram.com/farmaciasassociadaspiratini/" target="_blank" class="m-1"><i class="fa-brands fa-instagram icone mr-2"></i></a>
             </nav>
         </div>
     </div>
